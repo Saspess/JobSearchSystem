f5b1206d76aec9ffb75c09de0166d814ba79a5f3
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2023-02-26 14:15:43 +0300

    Add di extension for repositories

---
+++ src/Microservices/AccountsMS/AccountsMS.Data/IoC/ServicesExtensions.cs
@@ -0,0 +1,19 @@
+﻿using AccountsMS.Data.Repositories.Contracts;
+using AccountsMS.Data.Repositories.Implementation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AccountsMS.Data.IoC
+{
+    public static class ServicesExtensions
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IOrganizationRepository, OrganizationRepository>();
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+            services.AddScoped<IEmployeeSkillRepository, EmployeeSkillRepository>();
+            services.AddScoped<ISkillRepository, SkillRepository>();
+
+            return services;
+        }
+    }
+}
