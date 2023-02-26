982b94b5206e4baaa86418a2854d7c68af9830d2
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2023-02-26 14:16:48 +0300

    Add repositories into di container

--- src/Microservices/AccountsMS/AccountsMS.WebApi/Program.cs
+++ src/Microservices/AccountsMS/AccountsMS.WebApi/Program.cs
@@ -1,8 +1,13 @@
+using AccountsMS.Data.IoC;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddRepositories();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
