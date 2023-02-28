3549db99f7a4ed0a98e5a6ed5f7a672fabc0ab8a
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2023-02-28 23:15:14 +0300

    Add global exception handling middleware

---
+++ src/Microservices/AccountsMS/AccountsMS.WebApi/Middleware/GlobalExceptionHandlingMiddleware.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Text.Json;
+
+namespace AccountsMS.WebApi.Middleware
+{
+    public class GlobalExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public GlobalExceptionHandlingMiddleware(RequestDelegate next, ILogger logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception exception)
+            {
+                var code = HttpStatusCode.InternalServerError;
+                var message = exception.Message;
+
+                await HandleExceptionAsync(context, code, message);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, HttpStatusCode code, string message)
+        {
+            _logger.LogError(message);
+
+            var result = JsonSerializer.Serialize(message);
+
+            var httpResponse = context.Response;
+            httpResponse.ContentType = "application/json";
+            httpResponse.StatusCode = (int)code;
+
+            await httpResponse.WriteAsync(result);
+        }
+    }
+}
