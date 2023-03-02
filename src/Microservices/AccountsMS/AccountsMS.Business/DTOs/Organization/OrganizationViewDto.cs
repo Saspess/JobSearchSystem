8c572c4174981fbd00326b606671a09c82dcc2f8
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2023-03-02 20:43:19 +0300

    Add dtos

---
+++ src/Microservices/AccountsMS/AccountsMS.Business/DTOs/Organization/OrganizationViewDto.cs
@@ -0,0 +1,10 @@
+﻿namespace AccountsMS.Business.DTOs.Organization
+{
+    public class OrganizationViewDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Hometown { get; set; } = null!;
+        public string Email { get; set; } = null!;
+    }
+}
---
+++ src/Microservices/AccountsMS/AccountsMS.Business/DTOs/Skill/SkillUpdateModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AccountsMS.Business.DTOs.Skill
+{
+    public class SkillUpdateModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
