fd5fcc5d9f56778407361fdab2571b2e4a43a220
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2023-03-03 11:06:23 +0300

    Add interfaces for services

---
+++ src/Microservices/AccountsMS/AccountsMS.Business/Services/Contracts/IEmployeeService.cs
@@ -0,0 +1,20 @@
+﻿using AccountsMS.Business.DTOs.Employee;
+using AccountsMS.Business.DTOs.EmployeeSkill;
+
+namespace AccountsMS.Business.Services.Contracts
+{
+    public interface IEmployeeService
+    {
+        Task<IEnumerable<EmployeeViewDto>> GetAllEmployeesAsync();
+        Task<IEnumerable<EmployeeViewDto>> GetEmployeesAsync(string skillName);
+        Task<IEnumerable<EmployeeSkillViewDto>> GetAllEmployeeSkillsAsync();
+        Task<EmployeeViewDto?> GetEmployeeByIdAsync(int id);
+        Task<EmployeeViewDto?> GetEmployeeByEmailAsync(string email);
+        Task<EmployeeViewDto?> CreateEmployeeAsync(EmployeeCreateDto employeeCreateDto);
+        Task AddEmployeeSkillAsync(int employeeId, int skillId);
+        Task UpdateEmployeeAsync(EmployeeUpdateDto employeeUpdateDto);
+        Task ConfirmEmployeeSkillAsync(int employeeId, int skillId);
+        Task DeleteEmployeeAsync(int id);
+        Task DeleteEmployeeSkillAsync(int employeeId, int skillId);
+    }
+}
---
+++ src/Microservices/AccountsMS/AccountsMS.Business/Services/Contracts/ISkillService.cs
@@ -0,0 +1,13 @@
+﻿using AccountsMS.Business.DTOs.Skill;
+
+namespace AccountsMS.Business.Services.Contracts
+{
+    public interface ISkillService
+    {
+        Task<IEnumerable<SkillViewDto>> GetAllSkillsAsync();
+        Task<SkillViewDto?> GetSkillById(int id);
+        Task<SkillViewDto?> CreateSkillAsync(SkillCreateDto skillCreateDto);
+        Task UpdateSkillAsync(SkillUpdateDto skillUpdateDto);
+        Task DeleteSkillAsync(int id);
+    }
+}
