3ef11585d38c355fa8535b7b023fa8e641174545
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2023-03-03 09:52:05 +0300

    Add GetEmployeesBySkillName method to employee repository

--- src/Microservices/AccountsMS/AccountsMS.Data/Repositories/Contracts/IEmployeeRepository.cs
+++ src/Microservices/AccountsMS/AccountsMS.Data/Repositories/Contracts/IEmployeeRepository.cs
@@ -5,6 +5,7 @@ namespace AccountsMS.Data.Repositories.Contracts
     public interface IEmployeeRepository
     {
         Task<IEnumerable<EmployeeModel>> GetAllEmployeesAsync();
+        Task<IEnumerable<EmployeeModel>> GetEmployeesBySkillNameAsync(string skillName);
         Task<EmployeeModel?> GetEmployeeByIdAsync(int id);
         Task<EmployeeModel?> GetEmployeeByEmailAsync(string email);
         Task<EmployeeModel?> CreateEmployeeAsync(EmployeeCreateModel employeeCreateModel);
