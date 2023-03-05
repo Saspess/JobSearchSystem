cec0286504fe02b7d9f7dc28ff49b835dd76c19c
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2023-03-06 02:39:05 +0300

    Add services

---
+++ src/Microservices/AccountsMS/AccountsMS.Business/Services/Implementation/EmployeeService.cs
@@ -0,0 +1,148 @@
+﻿using AccountsMS.Business.DTOs.Employee;
+using AccountsMS.Business.DTOs.EmployeeSkill;
+using AccountsMS.Business.Exceptions;
+using AccountsMS.Business.Services.Contracts;
+using AccountsMS.Data.Models.Employee;
+using AccountsMS.Data.Repositories.Contracts;
+using AutoMapper;
+
+namespace AccountsMS.Business.Services.Implementation
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly IEmployeeSkillRepository _employeeSkillRepository;
+        private readonly ISkillRepository _skillRepository;
+        private readonly IMapper _mapper;
+
+        public EmployeeService(IEmployeeRepository employeeRepository, IEmployeeSkillRepository employeeSkillRepository, ISkillRepository skillRepository, IMapper mapper)
+        {
+            _employeeRepository = employeeRepository;
+            _employeeSkillRepository = employeeSkillRepository;
+            _skillRepository = skillRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<EmployeeViewDto>> GetAllEmployeesAsync()
+        {
+            var employeesModels = await _employeeRepository.GetAllEmployeesAsync();
+
+            var employeesViewDtos = _mapper.Map<IEnumerable<EmployeeViewDto>>(employeesModels);
+
+            return employeesViewDtos;
+        }
+
+        public async Task<IEnumerable<EmployeeViewDto>> GetEmployeesBySkillNameAsync(string skillName)
+        {
+            var employeesModels = await _employeeRepository.GetEmployeesBySkillNameAsync(skillName);
+
+            var employeesViewDtos = _mapper.Map<IEnumerable<EmployeeViewDto>>(employeesModels);
+
+            return employeesViewDtos;
+        }
+
+        public async Task<IEnumerable<EmployeeSkillViewDto>> GetAllEmployeeSkillsAsync(int id)
+        {
+            var employeeModel = await _employeeRepository.GetEmployeeByIdAsync(id)
+                ?? throw new NotFoundException("Employee was not found.");
+
+            var employeeSkillsModels = await _employeeSkillRepository.GetAllEmployeeSkillsAsync(id);
+
+            var employeeSkillsViewDtos = _mapper.Map<IEnumerable<EmployeeSkillViewDto>>(employeeSkillsModels);
+
+            return employeeSkillsViewDtos;
+        }
+
+        public async Task<EmployeeViewDto> GetEmployeeByIdAsync(int id)
+        {
+            var employeeModel = await _employeeRepository.GetEmployeeByIdAsync(id)
+                ?? throw new NotFoundException("Employee was not found.");
+
+            var employeeViewDto = _mapper.Map<EmployeeViewDto>(employeeModel);
+
+            return employeeViewDto;
+        }
+
+        public async Task<EmployeeViewDto> GetEmployeeByEmailAsync(string email)
+        {
+            var employeeModel = await _employeeRepository.GetEmployeeByEmailAsync(email)
+                ?? throw new NotFoundException("Employee was not found.");
+
+            var employeeViewDto = _mapper.Map<EmployeeViewDto>(employeeModel);
+
+            return employeeViewDto;
+        }
+
+        public async Task<EmployeeViewDto> CreateEmployeeAsync(EmployeeCreateDto employeeCreateDto)
+        {
+            if (employeeCreateDto == null)
+            {
+                throw new ArgumentNullException(nameof(employeeCreateDto));
+            }
+
+            var employeeCreateModel = _mapper.Map<EmployeeCreateModel>(employeeCreateDto);
+
+            var createdEmployeeModel = await _employeeRepository.CreateEmployeeAsync(employeeCreateModel);
+
+            var createdEmployeeViewDto = _mapper.Map<EmployeeViewDto>(createdEmployeeModel);
+
+            return createdEmployeeViewDto;
+        }
+
+        public async Task AddEmployeeSkillAsync(int employeeId, int skillId)
+        {
+            var existingEmployeeModel = await _employeeRepository.GetEmployeeByIdAsync(employeeId)
+                ?? throw new NotFoundException("Employee was not found.");
+
+            var existingSkillModel = await _skillRepository.GetSkillByIdAsync(skillId)
+                ?? throw new NotFoundException("Skill was not found.");
+
+            await _employeeSkillRepository.AddEmployeeSkillAsync(employeeId, skillId);
+        }
+
+        public async Task UpdateEmployeeAsync(EmployeeUpdateDto employeeUpdateDto)
+        {
+            if (employeeUpdateDto == null)
+            {
+                throw new ArgumentNullException(nameof(employeeUpdateDto));
+            }
+
+            var existingEmployeeModel = await _employeeRepository.GetEmployeeByIdAsync(employeeUpdateDto.Id)
+                ?? throw new NotFoundException("Employee was not found.");
+
+            var employeeUpdateModel = _mapper.Map<EmployeeUpdateModel>(employeeUpdateDto);
+
+            await _employeeRepository.UpdateEmployeeAsync(employeeUpdateModel);
+        }
+
+        public async Task ConfirmEmployeeSkillAsync(int employeeId, int skillId)
+        {
+            var existingEmployeeModel = await _employeeRepository.GetEmployeeByIdAsync(employeeId)
+                ?? throw new NotFoundException("Employee was not found.");
+
+            var existingSkillModel = await _skillRepository.GetSkillByIdAsync(skillId)
+                ?? throw new NotFoundException("Skill was not found.");
+
+            await _employeeSkillRepository.ConfirmEmployeeSkillAsync(employeeId, skillId);
+        }
+
+        public async Task DeleteEmployeeAsync(int id)
+        {
+            var existingEmployeeModel = await _employeeRepository.GetEmployeeByIdAsync(id)
+                ?? throw new NotFoundException("Employee was not found.");
+
+            await _employeeRepository.DeleteEmployeeAsync(id);
+        }
+
+        public async Task DeleteEmployeeSkillAsync(int employeeId, int skillId)
+        {
+            var existingEmployeeModel = await _employeeRepository.GetEmployeeByIdAsync(employeeId)
+                ?? throw new NotFoundException("Employee was not found.");
+
+            var existingSkillModel = await _skillRepository.GetSkillByIdAsync(skillId)
+                ?? throw new NotFoundException("Skill was not found.");
+
+            await _employeeSkillRepository.DeleteEmployeeSkillAsync(employeeId, skillId);
+        }
+    }
+}
---
+++ src/Microservices/AccountsMS/AccountsMS.Business/Services/Implementation/SkillService.cs
@@ -0,0 +1,79 @@
+﻿using AccountsMS.Business.DTOs.Skill;
+using AccountsMS.Business.Exceptions;
+using AccountsMS.Business.Services.Contracts;
+using AccountsMS.Data.Models.Skill;
+using AccountsMS.Data.Repositories.Contracts;
+using AutoMapper;
+
+namespace AccountsMS.Business.Services.Implementation
+{
+    public class SkillService : ISkillService
+    {
+        private readonly ISkillRepository _skillRepository;
+        private readonly IMapper _mapper;
+
+        public SkillService(ISkillRepository skillRepository, IMapper mapper)
+        {
+            _skillRepository = skillRepository ?? throw new ArgumentNullException(nameof(skillRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        public async Task<IEnumerable<SkillViewDto>> GetAllSkillsAsync()
+        {
+            var skillsModels = await _skillRepository.GetAllSkillsAsync();
+
+            var skillsViewDtos = _mapper.Map<IEnumerable<SkillViewDto>>(skillsModels);
+
+            return skillsViewDtos;
+        }
+
+        public async Task<SkillViewDto> GetSkillByIdAsync(int id)
+        {
+            var skillModel = await _skillRepository.GetSkillByIdAsync(id)
+                ?? throw new NotFoundException("Skill was not found.");
+
+            var skillViewDto = _mapper.Map<SkillViewDto>(skillModel);
+
+            return skillViewDto;
+        }
+
+        public async Task<SkillViewDto> CreateSkillAsync(SkillCreateDto skillCreateDto)
+        {
+            if (skillCreateDto == null)
+            {
+                throw new ArgumentNullException(nameof(skillCreateDto));
+            }
+
+            var skillCreateModel = _mapper.Map<SkillCreateModel>(skillCreateDto);
+
+            var createdSkillModel = await _skillRepository.CreateSkillAsync(skillCreateModel);
+
+            var createdSkillViewDto = _mapper.Map<SkillViewDto>(createdSkillModel);
+
+            return createdSkillViewDto;
+        }
+
+        public async Task UpdateSkillAsync(SkillUpdateDto skillUpdateDto)
+        {
+            if (skillUpdateDto == null)
+            {
+                throw new ArgumentNullException(nameof(skillUpdateDto));
+            }
+
+            var existingSkillModel = await _skillRepository.GetSkillByIdAsync(skillUpdateDto.Id)
+                ?? throw new NotFoundException("Skill was not found.");
+
+            var skillUpdateModel = _mapper.Map<SkillUpdateModel>(skillUpdateDto);
+
+            await _skillRepository.UpdateSkillAsync(skillUpdateModel);
+        }
+
+        public async Task DeleteSkillAsync(int id)
+        {
+            var existingSkillModel = await _skillRepository.GetSkillByIdAsync(id)
+                ?? throw new NotFoundException("Skill was not found.");
+
+            await _skillRepository.DeleteSkillAsync(id);
+        }
+    }
+}
