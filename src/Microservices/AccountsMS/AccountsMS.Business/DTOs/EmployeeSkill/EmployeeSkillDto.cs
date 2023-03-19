4df4c4bc8e774ef651cce267f99dfbea369e6946
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2023-03-19 21:16:45 +0300

    Add EmployeeSkillDto

---
+++ src/Microservices/AccountsMS/AccountsMS.Business/DTOs/EmployeeSkill/EmployeeSkillDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AccountsMS.Business.DTOs.EmployeeSkill
+{
+    public class EmployeeSkillDto
+    {
+        public int EmployeeId { get; set; }
+        public int SkillId { get; set;}
+    }
+}
