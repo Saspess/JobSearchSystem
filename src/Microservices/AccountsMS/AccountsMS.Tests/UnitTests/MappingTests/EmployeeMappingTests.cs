5b165d5b06288466c07eb7e6c9f8b4fb899f4cdb
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2023-03-27 10:33:18 +0300

    Add mapping tests

---
+++ src/Microservices/AccountsMS/AccountsMS.Tests/UnitTests/MappingTests/EmployeeMappingTests.cs
@@ -0,0 +1,22 @@
+﻿using AccountsMS.Business.MappingProfiles;
+using AutoMapper;
+
+namespace AccountsMS.Tests.UnitTests.MappingTests
+{
+    public class EmployeeMappingTests
+    {
+        [Fact]
+        public void ValidateEmployeeMappingProfile_ShouldReturnSuccessResult()
+        {
+            var mapperConfig = new MapperConfiguration(
+            cfg =>
+            {
+                cfg.AddProfile(new EmployeeMappingProfile());
+            });
+
+            IMapper mapper = new Mapper(mapperConfig);
+
+            mapper.ConfigurationProvider.AssertConfigurationIsValid();
+        }
+    }
+}
---
+++ src/Microservices/AccountsMS/AccountsMS.Tests/UnitTests/MappingTests/EmployeeSkillMappingTests.cs
@@ -0,0 +1,22 @@
+﻿using AccountsMS.Business.MappingProfiles;
+using AutoMapper;
+
+namespace AccountsMS.Tests.UnitTests.MappingTests
+{
+    public class EmployeeSkillMappingTests
+    {
+        [Fact]
+        public void ValidateEmployeeSkillMappingProfile_ShouldReturnSuccessResult()
+        {
+            var mapperConfig = new MapperConfiguration(
+            cfg =>
+            {
+                cfg.AddProfile(new EmployeeSkillMappingProfile());
+            });
+
+            IMapper mapper = new Mapper(mapperConfig);
+
+            mapper.ConfigurationProvider.AssertConfigurationIsValid();
+        }
+    }
+}
