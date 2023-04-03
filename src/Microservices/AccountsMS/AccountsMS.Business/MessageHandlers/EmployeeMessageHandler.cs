c653b30ceb9400640217b71e7766beae27e8710e
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2023-04-03 22:45:24 +0300

    Add message handlers

---
+++ src/Microservices/AccountsMS/AccountsMS.Business/MessageHandlers/EmployeeMessageHandler.cs
@@ -0,0 +1,43 @@
+﻿using AccountsMS.Business.DTOs.Employee;
+using AccountsMS.Business.Exceptions;
+using AccountsMS.Data.Models.Employee;
+using AccountsMS.Data.Repositories.Contracts;
+using AutoMapper;
+using Kafka.Contracts;
+
+namespace AccountsMS.Business.MessageHandlers
+{
+    public class EmployeeMessageHandler : IKafkaConsumerHandler<string, EmployeeMessageDto>
+    {
+        private IEmployeeRepository _employeeRepository;
+        private IOrganizationRepository _organizationRepository;
+        private IMapper _mapper;
+
+        public EmployeeMessageHandler(IEmployeeRepository employeeRepository, IOrganizationRepository organizationRepository,IMapper mapper)
+        {
+            _employeeRepository = employeeRepository;
+            _organizationRepository = organizationRepository;
+            _mapper = mapper;
+        }
+
+        public async Task HandleAsync(string key, EmployeeMessageDto value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            var existingEmployee = _employeeRepository.GetEmployeeByEmailAsync(value.Email)
+                ?? throw new AlreadyExistsException("User already exists.");
+
+            if (value.OrganizationId != null)
+            {
+                var existingOrganization = _organizationRepository.GetOrganizationByIdAsync((int)value.OrganizationId) ?? throw new NotFoundException("Organization was not found.");
+            }
+
+            var employeeCreateModel = _mapper.Map<EmployeeCreateModel>(value);
+
+            await _employeeRepository.CreateEmployeeAsync(employeeCreateModel);
+        }
+    }
+}
---
+++ src/Microservices/AccountsMS/AccountsMS.Business/MessageHandlers/OrganizationMessageHandler.cs
@@ -0,0 +1,38 @@
+﻿using AccountsMS.Business.DTOs.Organization;
+using AccountsMS.Business.Exceptions;
+using AccountsMS.Data.Models.Employee;
+using AccountsMS.Data.Models.Organization;
+using AccountsMS.Data.Repositories.Contracts;
+using AccountsMS.Data.Repositories.Implementation;
+using AutoMapper;
+using Kafka.Contracts;
+
+namespace AccountsMS.Business.MessageHandlers
+{
+    public class OrganizationMessageHandler : IKafkaConsumerHandler<string, OrganizationMessageDto>
+    {
+        private IOrganizationRepository _organizationRepository;
+        private IMapper _mapper;
+
+        public OrganizationMessageHandler(IOrganizationRepository organizationRepository, IMapper mapper)
+        {
+            _organizationRepository = organizationRepository;
+            _mapper = mapper;
+        }
+
+        public async Task HandleAsync(string key, OrganizationMessageDto value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            var existingOrganization = _organizationRepository.GetOrganizationByEmailAsync(value.Email)
+                ?? throw new AlreadyExistsException("Organization already exists.");
+
+            var organizationCreateModel = _mapper.Map<OrganizationCreateModel>(value);
+
+            await _organizationRepository.CreateOrganizationAsync(organizationCreateModel);
+        }
+    }
+}
