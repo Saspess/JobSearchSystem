81466350e611f25163ff5eb3af2d98394ceced0a
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2023-04-05 12:16:59 +0300

    Add controllers

---
+++ src/Microservices/IdentityMS/IdentityMS.WebApi/Controllers/BaseIdentityMSController.cs
@@ -0,0 +1,48 @@
+﻿using IdentityMS.Business.Response.Enums;
+using IdentityMS.WebApi.Response.Enums;
+using IdentityMS.WebApi.Response.Generic;
+using IdentityMS.WebApi.Response.NonGeneric;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace IdentityMS.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public abstract class BaseIdentityMSController : ControllerBase
+    {
+        [NonAction]
+        public JssResult Succesed()
+        {
+            return new JssResult(HttpStatusCode.OK, IdentityMSResultCodes.Successed);
+        }
+
+        [NonAction]
+        public JssResult<T> Succesed<T>(T value)
+        {
+            return new JssResult<T>(HttpStatusCode.OK, IdentityMSResultCodes.Successed, value);
+        }
+
+        [NonAction]
+        public JssResult Failed(Enum code)
+        {
+            if (code is IdentityMSErrorCodes.EntityNotFound)
+            {
+                return new JssResult(HttpStatusCode.NotFound, code);
+            }
+
+            return new JssResult(HttpStatusCode.BadRequest, code);
+        }
+
+        [NonAction]
+        public JssResult<T> Failed<T>(Enum code)
+        {
+            if (code is IdentityMSErrorCodes.EntityNotFound)
+            {
+                return new JssResult<T>(HttpStatusCode.NotFound, code);
+            }
+
+            return new JssResult<T>(HttpStatusCode.BadRequest, code);
+        }
+    }
+}
---
+++ src/Microservices/IdentityMS/IdentityMS.WebApi/Controllers/UserController.cs
@@ -0,0 +1,103 @@
+﻿using IdentityMS.Business.DTOs.Employee;
+using IdentityMS.Business.DTOs.Organization;
+using IdentityMS.Business.DTOs.User;
+using IdentityMS.Business.Services.Contracts;
+using IdentityMS.WebApi.Response.Generic;
+using IdentityMS.WebApi.Response.NonGeneric;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityMS.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : BaseIdentityMSController
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<JssResult<IEnumerable<UserViewDto>>> GetAllUsersAsync()
+        {
+            var result = await _userService.GetAllUsersAsync();
+
+            if (result.IsSuccess == false)
+            {
+                return Failed<IEnumerable<UserViewDto>>(result.ErrorCode);
+            }
+
+            return Succesed<IEnumerable<UserViewDto>>(result.Value);
+        }
+
+        [HttpPost]
+        [Route("RegisterEmployee")]
+        public async Task<JssResult<UserViewDto>> RegisterEmployeeAsync([FromBody] EmployeeRegisterDto employeeRegisterDto)
+        {
+            var result = await _userService.RegisterEmployeeAsync(employeeRegisterDto);
+
+            if (result.IsSuccess == false)
+            {
+                return Failed<UserViewDto>(result.ErrorCode);
+            }
+
+            return Succesed<UserViewDto>(result.Value);
+        }
+
+        [HttpPost]
+        [Route("RegisterOrganization")]
+        public async Task<JssResult<UserViewDto>> RegisterOrganizationAsync([FromBody] OrganizationRegisterDto organizationRegisterDto)
+        {
+            var result = await _userService.RegisterOrganizationAsync(organizationRegisterDto);
+
+            if (result.IsSuccess == false)
+            {
+                return Failed<UserViewDto>(result.ErrorCode);
+            }
+
+            return Succesed<UserViewDto>(result.Value);
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<JssResult<string>> LoginAsync([FromBody] UserLoginDto userLoginDto)
+        {
+            var result = await _userService.LoginAsync(userLoginDto);
+
+            if (result.IsSuccess == false)
+            {
+                return Failed<string>(result.ErrorCode);
+            }
+
+            return Succesed<string>(result.Value);
+        }
+
+        [HttpPut]
+        public async Task<JssResult> UpdateUserAsync([FromBody] UserUpdateDto userUpdateDto)
+        {
+            var result = await _userService.UpdateUserAsync(userUpdateDto);
+
+            if (result.IsSuccess == false)
+            {
+                return Failed(result.ErrorCode);
+            }
+
+            return Succesed();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<JssResult> DeleteUserAsync([FromRoute] int id)
+        {
+            var result = await _userService.DeleteUserAsync(id);
+
+            if(result.IsSuccess == false)
+            {
+                return Failed(result.ErrorCode);
+            }
+
+            return Succesed();
+        }
+    }
+}
