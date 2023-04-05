1a673c9e5375190076224c720f107178b6e7ed91
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2023-04-05 12:46:33 +0300

    Configure authentication

--- src/Microservices/IdentityMS/IdentityMS.WebApi/Program.cs
+++ src/Microservices/IdentityMS/IdentityMS.WebApi/Program.cs
@@ -1,6 +1,10 @@
 using IdentityMS.Business.IoC;
 using IdentityMS.Business.SeedData;
 using IdentityMS.Data.IoC;
+using IdentityMS.WebApi.Middleware;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,12 +18,30 @@ builder.Services.ConfigureSqlContext(builder.Configuration)
     .ConfigureServices()
     .ConfigureProducers();
 
+builder.Services.AddAuthentication(auth =>
+{
+    auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+        ValidateIssuerSigningKey = true
+    }
+ );
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseMiddleware<GlobalExceptionHandlingMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -31,6 +53,8 @@ if (app.Environment.IsDevelopment())
 await app.SeedDataAsync();
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
