86d1f29f382ea662c2dcb98d6f720cf4d40cf22b
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2023-04-07 10:57:09 +0300

    Add db context

---
+++ src/Microservices/VacanciesMS/VacanciesMS.Data/Contexts/Contracts/IApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VacanciesMS.Data.Entities;
+
+namespace VacanciesMS.Data.Contexts.Contracts
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<OrganizationEntity> Organizations { get; set; }
+        DbSet<VacancyEntity> Vacancies { get; set; }
+        DbSet<RespondEntity> Responds { get; set; }
+        DbSet<EmployeeEntity> Employees { get; set; }
+
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+
+        Task<int> SaveChangesAsync();
+    }
+}
