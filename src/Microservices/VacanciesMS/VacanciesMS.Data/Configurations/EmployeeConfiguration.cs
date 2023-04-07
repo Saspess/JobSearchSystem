4071cc30f7ca7747ecaa7f6613ca6f4b19fc1aff
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2023-04-07 10:51:03 +0300

    Add configurations

---
+++ src/Microservices/VacanciesMS/VacanciesMS.Data/Configurations/EmployeeConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VacanciesMS.Data.Entities;
+
+namespace VacanciesMS.Data.Configurations
+{
+    public class EmployeeConfiguration : IEntityTypeConfiguration<EmployeeEntity>
+    {
+        public void Configure(EntityTypeBuilder<EmployeeEntity> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.Id).UseIdentityColumn(1, 1).ValueGeneratedOnAdd();
+
+            builder.Property(e => e.FirstName).HasMaxLength(50).IsRequired();
+
+            builder.Property(e => e.LastName).HasMaxLength(50).IsRequired();
+
+            builder.Property(e => e.Email).HasMaxLength(50).IsRequired();
+        }
+    }
+}
---
+++ src/Microservices/VacanciesMS/VacanciesMS.Data/Configurations/OrganizationConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VacanciesMS.Data.Entities;
+
+namespace VacanciesMS.Data.Configurations
+{
+    public class OrganizationConfiguration : IEntityTypeConfiguration<OrganizationEntity>
+    {
+        public void Configure(EntityTypeBuilder<OrganizationEntity> builder)
+        {
+            builder.HasKey(o => o.Id);
+            builder.Property(o => o.Id).UseIdentityColumn(1, 1).ValueGeneratedOnAdd();
+
+            builder.Property(o => o.Name).HasMaxLength(100).IsRequired();
+
+            builder.Property(o => o.Email).HasMaxLength(50).IsRequired();
+        }
+    }
+}
