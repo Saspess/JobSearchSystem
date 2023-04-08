5a4d81c46547d1528de83baad6ce9dc2bd3f5e69
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2023-04-08 22:32:02 +0300

    Add repositories

---
+++ src/Microservices/VacanciesMS/VacanciesMS.Data/Repositories/Implementation/BaseRepository.cs
@@ -0,0 +1,49 @@
+﻿using VacanciesMS.Data.Contexts.Contracts;
+using VacanciesMS.Data.Entities;
+using VacanciesMS.Data.Repositories.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace VacanciesMS.Data.Repositories.Implementation
+{
+    public abstract class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : BaseEntity
+    {
+        protected IApplicationDbContext appContext;
+
+        public BaseRepository(IApplicationDbContext appContext)
+        {
+            this.appContext = appContext;
+        }
+
+        public virtual async Task<IEnumerable<TEntity>> GetAllAsync() =>
+           await appContext.Set<TEntity>()
+           .AsNoTracking()
+           .ToListAsync();
+
+        public virtual async Task<TEntity?> GetByIdAsync(int id) =>
+            await appContext.Set<TEntity>()
+            .AsNoTracking()
+            .FirstOrDefaultAsync(e => e.Id == id);
+
+        public virtual async Task<TEntity> CreateAsync(TEntity entity)
+        {
+            var created = await appContext.Set<TEntity>().AddAsync(entity);
+            await appContext.SaveChangesAsync();
+
+            return created.Entity;
+        }
+
+        public virtual async Task UpdateAsync(TEntity entity)
+        {
+            appContext.Set<TEntity>().Update(entity);
+            await appContext.SaveChangesAsync();
+        }
+
+        public virtual async Task DeleteAsync(int id)
+        {
+            var entity = await appContext.Set<TEntity>().FindAsync(id);
+
+            appContext.Set<TEntity>().Remove(entity);
+            await appContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ src/Microservices/VacanciesMS/VacanciesMS.Data/Repositories/Implementation/VacancyRepository.cs
@@ -0,0 +1,11 @@
+﻿using VacanciesMS.Data.Contexts.Contracts;
+using VacanciesMS.Data.Entities;
+using VacanciesMS.Data.Repositories.Contracts;
+
+namespace VacanciesMS.Data.Repositories.Implementation
+{
+    public class VacancyRepository : BaseRepository<VacancyEntity>, IVacancyRepository
+    {
+        public VacancyRepository(IApplicationDbContext appContext) : base(appContext) { }
+    }
+}
